86d944e4b98f8aad81dca11047375922222eb865
ANIRBITADAK <anirbitadak@gmail.com>
2021-08-03 10:43:51 +0530

    First commit

---
+++ app/src/main/java/com/tux/dms/MainActivity.java
@@ -0,0 +1,27 @@
+package com.tux.dms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+
+                Intent mainIntent = new Intent(MainActivity.this,login.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
