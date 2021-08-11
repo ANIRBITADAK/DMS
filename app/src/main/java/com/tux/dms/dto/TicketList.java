3b65f4c53ed353abc9b44202ccaa87cc4a42c7e0
Ayan Adak <aadak@vmware.com>
2021-08-12 01:01:57 +0530

    intigrate GET ticket api

--- app/src/main/java/com/tux/dms/dto/TicketList.java
+++ app/src/main/java/com/tux/dms/dto/TicketList.java
@@ -1,4 +1,43 @@
 package com.tux.dms.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TicketList {
+    int total;
+    int totalPages;
+    int currentPage;
+    List<Ticket> tickets;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
 }
--- app/src/main/java/com/tux/dms/rest/ApiInterface.java
+++ app/src/main/java/com/tux/dms/rest/ApiInterface.java
@@ -3,6 +3,7 @@ package com.tux.dms.rest;
 import com.tux.dms.dto.JWTToken;
 
 import com.tux.dms.dto.Ticket;
+import com.tux.dms.dto.TicketList;
 import com.tux.dms.dto.User;
 import com.tux.dms.dto.UserCredential;
 
@@ -14,6 +15,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 
 public interface ApiInterface {
@@ -22,11 +24,16 @@ public interface ApiInterface {
     Call<JWTToken> executeLogin(@Body UserCredential credential);
 
     @POST("/api/users")
-    Call<JWTToken> executeSignup (@Body User user);
+    Call<JWTToken> executeSignup(@Body User user);
 
     @GET("/api/auth")
     Call<User> getUser(@Header("x-auth-token") String authHeader);
 
     @POST("api/tickets/user/{user_id}")
     Call<Ticket> createTicket(@Body Ticket ticket, @Path("user_id") String user_id);
+
+    @GET("api/tickets")
+    Call<TicketList> getTickets(@Header("x-auth-token") String authHeader, @Query("assignedTo") String assignedTo, @Query("state") String state,
+                                @Query("priority") Integer priority,
+                                @Query("page") Integer page, @Query("limit") Integer limit);
 }
