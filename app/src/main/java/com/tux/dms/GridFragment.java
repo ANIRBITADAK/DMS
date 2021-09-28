5a220765d2b02418c3a07c27a2e5ca194c6db69f
ANIRBIT ADAK <anirbitadak@ANIRBITs-MacBook-Air.local>
2021-09-28 18:08:50 +0530

    creating showPDF fragment

--- app/src/main/java/com/tux/dms/GridFragment.java
+++ app/src/main/java/com/tux/dms/GridFragment.java
@@ -1,7 +1,5 @@
 package com.tux.dms;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,26 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import com.squareup.picasso.Picasso;
-import com.tux.dms.constants.TicketConst;
-import com.tux.dms.rest.ApiClient;
-import com.tux.dms.rest.DownloadImageTask;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ShowImageFragment#newInstance} factory method to
+ * Use the {@link GridFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ShowImageFragment extends Fragment {
-
-    ImageView imageView;
+public class GridFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -39,7 +24,7 @@ public class ShowImageFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ShowImageFragment() {
+    public GridFragment() {
         // Required empty public constructor
     }
 
@@ -49,11 +34,11 @@ public class ShowImageFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ShowImageFragment.
+     * @return A new instance of fragment GridFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ShowImageFragment newInstance(String param1, String param2) {
-        ShowImageFragment fragment = new ShowImageFragment();
+    public static GridFragment newInstance(String param1, String param2) {
+        GridFragment fragment = new GridFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -73,19 +58,7 @@ public class ShowImageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_show_image, container, false);
-        imageView = view.findViewById(R.id.showImgView);
-
-
-        Bundle ticketImageBundle = getArguments();
-        String imagePath = (String) ticketImageBundle.get(TicketConst.TICKET_IMG_PATH);
-        if (imagePath != null) {
-            String imageUrl = "http://" + ApiClient.getIpAddress() + ":" + ApiClient.getPORT() + imagePath;
-            new DownloadImageTask(imageView)
-                    .execute(imageUrl);
-        }
-
-
-        return view;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_grid, container, false);
     }
 }
